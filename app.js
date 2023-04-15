4e25eb8184c3f35c4741f717bea5ecdd77a126fe
Andrea Malone <andreamurry63@gmail.com>
2023-04-15 17:28:34 -0500

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"    
+let answerOne = prompt("Am I From Tennessee").toLowerCase()
+
+ if (answerOne === "yes" || answerOne === "y") {
+ alert("Yes I Am From Tennessee")
+
+} else {
+alert("That Is Wrong I Am From Tennessee")
+}
+let answerTwo = prompt ("Do you live in Chapel Hill")
